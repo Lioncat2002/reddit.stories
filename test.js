ab2848e330ed2d3b23cf0250d0be0bb5b4b03ee9
Lioncat2002 <sankhayan2002@gmail.com>
2021-12-09 18:58:57 +0530

    updates

--- test.js
+++ test.js
@@ -1,16 +1,5 @@
-import story from './reddit/stories.js'
-/**
- * Showing the eample usage
- */
-story
-	//.FetchPostbyID(
-	//	"g17pmr"
-	//		)
-	.FetchPostbyLink(
-		"https://www.reddit.com/r/freehorrorstories/comments/g17pmr/creepy_pasta_true_story_wanted_for_podcast_episode"
-		
-	)
-	.then((data) => {
-		console.log('FetchPost Example:');
-		console.log(data);
-	}); 
+const { reddit } = require('./src/reddit');
+reddit.FetchPostbyID('nwccti').then((data) => {
+	console.log('FetchPostbyID Example:');
+	console.log(data);
+});
